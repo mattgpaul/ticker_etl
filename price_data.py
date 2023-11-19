34e8a5d073303518bbbb1cb4e36827037a525299
Matthew Paul <mattgpaul@gmail.com>
2023-11-19 10:57:41 -0700

    downloading daily data for a list of tickers

--- price_data.py
+++ price_data.py
@@ -2,13 +2,13 @@
 # Imports
 import pandas as pd
 import numpy as np
+import yfinance as yf
+from typing import List
 
 # User Defined Functions
-def get_tickers():
-    pass
-
-def get_ticker_data():
-    pass
+def get_ticker_data(tickers: List):
+    df = yf.download(tickers, period='1d', threads=True)
+    return df
 
 def organize_ticker_data():
     pass
@@ -16,9 +16,11 @@ def organize_ticker_data():
 def ingest_ticker_data():
     pass
 
-def execute_pipeline():
-    pass
+def execute_pipeline(tickers):
+    prices = get_ticker_data(tickers)
+    print(prices.head())
 
 # Script Execution
 if __name__ == "__main__":
-    pass
+    tickers = ['AAPL','GOOG','TSLA','MSFT','NVDA','AMD']
+    execute_pipeline(tickers)
