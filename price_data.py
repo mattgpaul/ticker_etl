b55ea1e890e75c1cffc458f0d71bfb8def478b2e
Matthew Paul <mattgpaul@gmail.com>
2023-11-19 10:35:32 -0700

    initial functions and structure defined

--- price_data.py
+++ price_data.py
@@ -1,1 +1,24 @@
 # Pipeline file for extracting and ingesting ticker price data
+# Imports
+import pandas as pd
+import numpy as np
+
+# User Defined Functions
+def get_tickers():
+    pass
+
+def get_ticker_data():
+    pass
+
+def organize_ticker_data():
+    pass
+
+def ingest_ticker_data():
+    pass
+
+def execute_pipeline():
+    pass
+
+# Script Execution
+if __name__ == "__main__":
+    pass
